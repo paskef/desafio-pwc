bb302d0b40b6323a2cea3ece4d57e8977f7b6b8c
paskef <fernandapaske1@gmail.com>
2024-02-15 01:30:33 -0300

    Atualização da lógica implementada para casos mais complexos
    Utilização de regex para resolver os casos complexos
    Split para organizar o caso 'No'

--- Desafio_PWC/src/desafio/Lógica.java
+++ Desafio_PWC/src/desafio/Lógica.java
@@ -1,27 +1,56 @@
 package desafio;
 
-// Importa a biblioteca de Arrays.
+// Importa classes
 
-import java.util.Arrays; 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Lógica {
 
-	    public String[] processarEndereco(String endereco) {
-	        String[] resultado = new String[2]; // Array para armazenar a rua e o número da rua.
+    public static String[] processarEndereco(String endereco) {
+        String[] resultado = new String[2];
 
-	        // Divisão da string de endereço em palavras utilizando split.
-	        String[] partes = endereco.split(" ");
+        // Verifica se a string contém "No"
+        if (endereco.contains("No")) {
+            // Divide a string em duas partes usando "No" como delimitador
+            String[] partes = endereco.split("No");
+            resultado[0] = partes[0].trim(); // Nome da rua
+            resultado[1] = "No " + partes[1].trim(); // Número da rua
+            
+            return resultado;
+        } else {
+        	
+        // Padrões de expressões regulares
+        String avPattern = "^(.*?)(?:(?:\\s*No\\s*\\d+)|(?:\\s*[bB]\\b)|$)";
+        String numberPattern = "(No\\s*\\d+|\\d+\\s?\\w?\\b)";
 
-	        // O último elemento deve ser o número da rua.
-	        resultado[1] = partes[partes.length - 1];
+        // Compilar padrões
+        Pattern patternAv = Pattern.compile(avPattern);
+        Pattern patternNumber = Pattern.compile(numberPattern);
 
-	        // A rua é formada pelas partes restantes.
-	        resultado[0] = String.join(" ", Arrays.copyOfRange(partes, 0, partes.length - 1));
+        // Procurar correspondências e adicionar à lista de endereços
+        Matcher matcherAv = patternAv.matcher(endereco);
+        Matcher matcherNumber = patternNumber.matcher(endereco);
 
-	        return resultado;
-	        
-	        //TODO: Casos mais complexos.
-	        
-	    }
-	}
+        if (matcherAv.find()) {
+            resultado[0] = matcherAv.group(1).trim();
+        } else {
+            resultado[0] = "";
+        }
+
+        if (matcherNumber.find()) {
+            resultado[1] = matcherNumber.group().trim();
+        } else {
+            resultado[1] = "";
+        }
+        }
+
+        // Remover vírgulas
+        resultado[0] = resultado[0].replaceAll("\\d+\\s?\\w?\\b,?\\s?", "").trim();
+
+        return resultado;
+    }
+}
+
+
 
